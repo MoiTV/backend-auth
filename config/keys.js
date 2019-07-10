58e5675e4cebf598d83bd61458eae84bc73aaf77
moises valdez <moisesvaldez84@yahoo.com>
2019-07-09 17:19:19 -0700

    backend auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://moi123:xbox360559!@moicluster-lg2g9.mongodb.net/test?retryWrites=true&w=majority',
+    googleClientID: '274887438929-m7qf84ql38hsrg2lof3vo8lb0ntc97jn.apps.googleusercontent.com',
+    googleClientSecret: 'ds7G6isj15hCW0FHAiOVer5M'
+}
